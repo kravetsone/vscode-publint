9ff304274a3fda828017a8eaf73623ccce280e1f
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-06-02 16:56:55 +0300

    feat: add jsonc-parser and improve diagnostic code

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,14 @@
-// @ts-expect-error
-import type { MessageType } from "publint";
 import * as vscode from "vscode";
-import { ANSI_COLORS_REGEXP } from "./utils";
+import {
+	ANSI_COLORS_REGEXP,
+	getRangeForKey,
+	publintToVSCodeMap,
+} from "./utils";
 
-export const publintToVSCodeMap: Record<
-	MessageType,
-	vscode.DiagnosticSeverity
-> = {
-	warning: vscode.DiagnosticSeverity.Warning,
-	error: vscode.DiagnosticSeverity.Error,
-	suggestion: vscode.DiagnosticSeverity.Hint,
-};
-
-export async function activate(context: vscode.ExtensionContext) {
+export async function activate() {
 	const { publint } = await import("publint");
 	const { formatMessage } = await import("publint/utils");
+	const jsonc = await import("jsonc-parser/lib/esm/main.js");
 	console.log("SOME");
 	const diagnosticCollection =
 		vscode.languages.createDiagnosticCollection("lints");
@@ -30,42 +24,29 @@ export async function activate(context: vscode.ExtensionContext) {
 			});
 			const text = document.getText();
 			const packageJSON = JSON.parse(text);
-			console.log(messages);
 
 			for (const message of messages) {
-				const startPos = document.positionAt(1);
-				const endPos = document.positionAt(2);
-				const range = new vscode.Range(startPos, endPos);
-				console.log(JSON.stringify(formatMessage(message, packageJSON)));
+				const range = getRangeForKey(jsonc, document, message.path);
 				const diagnostic = new vscode.Diagnostic(
 					range,
-					formatMessage(message, packageJSON)?.replaceAll(
-						ANSI_COLORS_REGEXP,
-						"",
-					) || message.code,
+					`${
+						formatMessage(message, packageJSON)?.replaceAll(
+							ANSI_COLORS_REGEXP,
+							"",
+						) || message.code
+					}`,
 					publintToVSCodeMap[message.type],
 				);
 
+				diagnostic.code = {
+					value: message.code,
+					target: vscode.Uri.parse(
+						`https://publint.dev/rules#${message.code.toLowerCase()}`,
+					),
+				};
+
 				diagnostics.push(diagnostic);
 			}
-
-			// const text = document.getText();
-
-			// const diagnostics: vscode.Diagnostic[] = [];
-
-			// const regex = /console\.log/g;
-			// let match;
-			// while ((match = regex.exec(text))) {
-			// 	const startPos = document.positionAt(match.index);
-			// 	const endPos = document.positionAt(match.index + match[0].length);
-			// 	const range = new vscode.Range(startPos, endPos);
-			// 	const diagnostic = new vscode.Diagnostic(
-			// 		range,
-			// 		"АААА ПАСХАЛКО",
-			// 		vscode.DiagnosticSeverity.Warning,
-			// 	);
-			// 	diagnostics.push(diagnostic);
-			// }
 
 			diagnosticCollection.set(document.uri, diagnostics);
 		} catch (error) {
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,38 @@
+// @ts-expect-error
+import type { MessageType } from "publint";
+import * as vscode from "vscode";
+
 // inspired by https://github.com/chalk/ansi-regex/blob/main/index.js#L1
 export const ANSI_COLORS_REGEXP =
 	// biome-ignore lint/suspicious/noControlCharactersInRegex: <explanation>
 	/[\u001B\u009B][[\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\d\/#&.:=?%@~_]+)*|[a-zA-Z\d]+(?:;[-a-zA-Z\d\/#&.:=?%@~_]*)*)?\u0007)|(?:(?:\d{1,4}(?:;\d{0,4})*)?[\dA-PR-TZcf-nq-uy=><~]))/g;
+
+export function getRangeForKey(
+	jsonc: typeof import("jsonc-parser/lib/esm/main"),
+	document: vscode.TextDocument,
+	path: string[],
+) {
+	const text = document.getText();
+	const node = jsonc.parseTree(text);
+
+	if (!node)
+		return new vscode.Range(document.positionAt(1), document.positionAt(2));
+
+	const keyNode = jsonc.findNodeAtLocation(node, path);
+
+	if (!keyNode)
+		return new vscode.Range(document.positionAt(1), document.positionAt(2));
+
+	const startPos = document.positionAt(keyNode.offset);
+	const endPos = document.positionAt(keyNode.offset + keyNode.length);
+	return new vscode.Range(startPos, endPos);
+}
+
+export const publintToVSCodeMap: Record<
+	MessageType,
+	vscode.DiagnosticSeverity
+> = {
+	warning: vscode.DiagnosticSeverity.Warning,
+	error: vscode.DiagnosticSeverity.Error,
+	suggestion: vscode.DiagnosticSeverity.Hint,
+};
