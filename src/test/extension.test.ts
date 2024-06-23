3250fbae30caaca10378c4531465f5a63be2c7e5
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-06-23 21:35:49 +0300

    chore: add tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,58 @@
-import * as assert from "node:assert";
+import assert from "node:assert";
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-import * as vscode from "vscode";
-// import * as myExtension from '../../extension';
+import fs from "node:fs";
+import path from "node:path";
+import vscode from "vscode";
+
+// !Why before is undefined...
 
 suite("Extension Test Suite", () => {
 	vscode.window.showInformationMessage("Start all tests.");
 
-	test("Sample test", () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	// before(async () => {
+	// 	const projectPath = path.join(process.cwd(), "project");
+	// 	console.log(projectPath);
+	// 	fs.mkdirSync(projectPath);
+	// 	fs.writeFileSync(
+	// 		path.join(projectPath, "package.json"),
+	// 		JSON.stringify({
+	// 			name: "test-package",
+	// 		}),
+	// 	);
+
+	// 	const workspace = vscode.workspace.getWorkspaceFolder(
+	// 		vscode.Uri.file(projectPath),
+	// 	);
+
+	// 	await activate();
+	// });
+
+	test("Get diagnostic errors for package.json", async () => {
+		// TODO: fix undefined before in mocha ??? and erase it
+		const projectPath = path.join(process.cwd(), "test-project");
+		console.log(projectPath);
+		if (!fs.existsSync(projectPath)) fs.mkdirSync(projectPath);
+		fs.writeFileSync(
+			path.join(projectPath, "package.json"),
+			JSON.stringify({
+				name: "test-package",
+			}),
+		);
+
+		vscode.workspace.getWorkspaceFolder(vscode.Uri.file(projectPath));
+		//	end-before-script
+
+		const file = await vscode.workspace.openTextDocument(
+			vscode.Uri.file(path.resolve(projectPath, "package.json")),
+		);
+
+		// TODO: find a better way
+		setTimeout(() => {
+			const diagnostic = vscode.languages.getDiagnostics(file.uri);
+
+			console.log("result", file, diagnostic);
+
+			assert.notStrictEqual(0, diagnostic.length);
+		}, 1000);
 	});
 });
