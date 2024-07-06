89003b0e19b0ad176dc654888921bee53c54e5a8
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-07-06 13:10:37 +0300

    chore: some improves and optimizations

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import path from "node:path";
 import * as vscode from "vscode";
-import { getRangeForKey, publintToVSCodeMap } from "./utils";
+import { getRangeForKey, isJSONParsable, publintToVSCodeMap } from "./utils";
 
 export async function activate() {
 	const { publint } = await import("publint");
@@ -11,40 +11,43 @@ export async function activate() {
 		vscode.languages.createDiagnosticCollection("lints");
 
 	async function updateDiagnostic(document: vscode.TextDocument) {
-		const config = vscode.workspace.getConfiguration("vscode-publint");
+		if (
+			!path.basename(document.uri.fsPath).includes("package.json") ||
+			document.languageId !== "json"
+		)
+			return;
 
+		const text = document.getText();
+		if (!isJSONParsable(text)) return;
+
+		const config = vscode.workspace.getConfiguration("vscode-publint");
 		const ignoredRules = config.get<string[]>("ignored-rules");
 
+		const diagnostics: vscode.Diagnostic[] = [];
+
+		const packageJSON = JSON.parse(text);
+
+		// private property can be boolean or boolean-string https://json.schemastore.org/package.json
+		if (
+			(packageJSON.private === true || packageJSON.private === "true") &&
+			config.get<boolean>("ignore-private-packages")
+		)
+			return diagnosticCollection.set(document.uri, []);
+
 		try {
-			if (
-				!path.basename(document.uri.fsPath).includes("package.json") ||
-				document.languageId !== "json"
-			)
-				return;
-
-			const diagnostics: vscode.Diagnostic[] = [];
-
 			const { messages } = await publint({
 				pkgDir: path.dirname(document.uri.fsPath),
 			});
 
-			const text = document.getText();
-			const packageJSON = JSON.parse(text);
-
-			// private property can be boolean or boolean-string https://json.schemastore.org/package.json
-			if (
-				(packageJSON.private === true || packageJSON.private === "true") &&
-				config.get<boolean>("ignore-private-packages")
-			)
-				return diagnosticCollection.set(document.uri, []);
+			const jsonTree = jsonc.parseTree(text);
 
 			for (const message of messages) {
 				if (ignoredRules?.includes(message.code)) continue;
 
-				const range = getRangeForKey(jsonc, document, message.path);
+				const range = getRangeForKey(jsonTree, jsonc, document, message.path);
 				const diagnostic = new vscode.Diagnostic(
 					range,
-					`${formatMessage(message, packageJSON) || message.code}`,
+					formatMessage(message, packageJSON) || message.code,
 					publintToVSCodeMap[message.type],
 				);
 
--- src/utils.ts
+++ src/utils.ts
@@ -3,23 +3,22 @@ import type { MessageType } from "publint";
 import * as vscode from "vscode";
 
 export function getRangeForKey(
+	jsonTree: ReturnType<typeof import("jsonc-parser").parseTree>,
 	jsonc: typeof import("jsonc-parser/lib/esm/main"),
 	document: vscode.TextDocument,
 	path: string[],
 ) {
-	const text = document.getText();
-	const node = jsonc.parseTree(text);
-
-	if (!node)
+	if (!jsonTree)
 		return new vscode.Range(document.positionAt(1), document.positionAt(2));
 
-	const keyNode = jsonc.findNodeAtLocation(node, path);
+	const keyNode = jsonc.findNodeAtLocation(jsonTree, path);
 
 	if (!keyNode)
 		return new vscode.Range(document.positionAt(1), document.positionAt(2));
 
 	const startPos = document.positionAt(keyNode.offset);
 	const endPos = document.positionAt(keyNode.offset + keyNode.length);
+
 	return new vscode.Range(startPos, endPos);
 }
 
@@ -31,3 +30,13 @@ export const publintToVSCodeMap: Record<
 	error: vscode.DiagnosticSeverity.Error,
 	suggestion: vscode.DiagnosticSeverity.Information,
 };
+
+export function isJSONParsable(str: string) {
+	try {
+		JSON.parse(str);
+	} catch (e) {
+		return false;
+	}
+
+	return true;
+}
